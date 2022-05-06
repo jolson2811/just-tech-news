a72bf243f5ada880ba74e5234a0609293d7ffed2
jolson2811 <jolson2811@gmail.com>
2022-05-05 20:21:34 -0600

    created login/sign up page

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.Secret,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 
 const exphbs = require('express-handlebars');
