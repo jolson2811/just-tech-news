44bbd7f3143e7af3bb27056296e5e00fcc1e9e1d
jolson2811 <jolson2811@gmail.com>
2022-05-07 22:59:19 -0600

    added comment/upvote feature for logged in users, remove buttons for logged out viewers

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -18,20 +19,18 @@ const sess = {
     })
 };
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 
-
-const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// turn on routes
-app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
